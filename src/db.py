aa04304ba4011d39cc4d98b701c8b62c55bab58c
duyvo241092 <duyvo241092@users.noreply.github.com>
2018-10-22 11:18:03 -0500

    20181023

---
+++ src/db.py
@@ -0,0 +1,111 @@
+import sqlite3
+
+
+def init_db():
+    db = sqlite3.connect('database.db')
+    db.execute('DROP TABLE IF EXISTS Users;')
+    db.execute('DROP TABLE IF EXISTS Posts;')
+    db.execute('CREATE TABLE Users ('
+               '  Id           INTEGER PRIMARY KEY AUTOINCREMENT,'
+               '  UserName     TEXT UNIQUE NOT NULL,'
+               '  Password     TEXT NOT NULL'
+               ');')
+    db.execute('CREATE TABLE Posts ('
+               '  Id           INTEGER PRIMARY KEY AUTOINCREMENT,'
+               '  Author       INTEGER NOT NULL,'
+               '  Time         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,'
+               '  Title        TEXT NOT NULL,'
+               '  Content         TEXT NOT NULL,'
+               '  FOREIGN KEY (Author) REFERENCES Users (id)'
+               ');')
+    db.execute('INSERT INTO Users (UserName,Password) '
+               'VALUES(?, ?)', ('root', 'root'))
+    db.commit()
+    db.execute('INSERT INTO Users (UserName,Password) '
+               'VALUES(?, ?)', ('root1', 'root'))
+    db.commit()
+    db.execute('INSERT INTO Posts (Author,  Title, Content)'
+               'VALUES(?, ?, ?)', ('0',  't1', 'c1'))
+    db.commit()
+    db.execute('INSERT INTO Posts (Author,  Title, Content)'
+               'VALUES(?, ?, ?)', ('1',  't2', 'c2'))
+    db.commit()
+    db.execute('INSERT INTO Posts (Author,  Title, Content)'
+               'VALUES(?, ?, ?)', ('1',  't3', 'c3'))
+    db.commit()
+    db.close()
+
+
+def fetch_user_all():
+    db = sqlite3.connect('database.db')
+    db.row_factory = sqlite3.Row
+    c = db.cursor()
+    record = c.execute('SELECT Id, UserName, Password FROM Users').fetchall()
+    db.close()
+    return [dict(row) for row in record]
+
+
+def fetch_user_id(username):
+    db = sqlite3.connect('database.db')
+    db.row_factory = sqlite3.Row
+    c = db.cursor()
+    record = c.execute('SELECT Id, UserName, Password FROM Users '
+                       'WHERE UserName = (?)', [(username)]).fetchall()
+    db.close()
+    return [dict(row) for row in record]
+
+
+def create_user(username, password):
+    db = sqlite3.connect('database.db')
+    db.execute('INSERT INTO Users (UserName,Password) VALUES(?, ?)', (username, password))
+    db.commit()
+    db.close()
+
+
+def fetch_blog_all(author):
+    db = sqlite3.connect('database.db')
+    db.row_factory = sqlite3.Row
+    c = db.cursor()
+    record = c.execute(
+        'SELECT p.Id, Title, Content, Time, Author, UserName'
+        ' FROM  Posts p JOIN Users u ON p.Author = u.Id'
+        ' WHERE p.Author = ?'
+        ' ORDER BY Time DESC', str(author)
+    ).fetchall()
+    db.close()
+    return [dict(row) for row in record]
+
+
+def fetch_blog_single(id, author):
+    db = sqlite3.connect('database.db')
+    db.row_factory = sqlite3.Row
+    c = db.cursor()
+    record = c.execute('SELECT p.Id, Title, Content, Time, Author, UserName'
+                       ' FROM  Posts p JOIN Users u ON p.Author = u.Id'
+                       ' WHERE p.Id = ? AND Author = ?'
+                       ' ORDER BY Time DESC', id, str(author)).fetchall()
+    db.close()
+    return [dict(row) for row in record]
+
+
+def insert_blog_single(author,  title, content):
+    db = sqlite3.connect('database.db')
+    db.execute('INSERT INTO Posts (Author,  Title, Content)'
+               'VALUES(?, ?, ?)', (author,  title, content))
+    db.commit()
+    db.close()
+
+
+def update_blog_single(id, author,  title, content):
+    db = sqlite3.connect('database.db')
+    db.execute('UPDATE Posts SET Author = ?,  Title = ?, Content = ? WHERE Id = ?',
+               (author,  title, content, id))
+    db.commit()
+    db.close()
+
+
+def delete_blog_single(id):
+    db = sqlite3.connect('database.db')
+    db.execute('DELETE FROM Posts WHERE Id = ?', id)
+    db.commit()
+    db.close()
---
+++ src/server.py
@@ -0,0 +1,103 @@
+import os
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from db import init_db, fetch_blog_all, fetch_blog_single, create_user, fetch_user_all, insert_blog_single, update_blog_single, delete_blog_single, fetch_user_id
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+@app.route('/Init')
+def Init():
+    init_db()
+    return ""
+
+
+@app.route('/blog/<id>')
+@app.route('/blog', defaults={'id': ''})
+def blog(id):
+    author = session['User'];
+    if '' != id:
+        return render_template("blog.html", data=fetch_blog_single(id, author))
+    else:
+        return render_template("blog.html", data=fetch_blog_all(author))
+
+
+@app.route('/edit/<id>')
+@app.route('/edit',  methods=['POST', 'GET'], defaults={'id': ''})
+def edit(id):
+    if request.method == 'POST':
+        author = session['User'];
+        title = request.form['title']
+        content = request.form['content']
+
+        # Check if delete request
+        try:
+            delete = request.form['delete']
+        except:
+            delete = None
+
+        # Check id is available
+        try:
+            id = request.form['id']
+            if not id.isdigit():
+                id = None
+        except:
+            id = None
+
+        if delete is not None:
+            delete_blog_single(id)
+            return render_template("blog.html", data=fetch_blog_all(author))
+        elif id is None:
+            insert_blog_single(author, title, content)
+            return render_template("blog.html", data=fetch_blog_all(author))
+        else:
+            update_blog_single(id, author, title, content)
+            return render_template("blog.html", data=fetch_blog_single(id, author))
+    else:
+        if '' != id:
+            return render_template("edit.html", data=fetch_blog_single(id)[0])
+        else:
+            return render_template("edit.html", data=None)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        if not username.isalnum():
+            return render_template("login.html", data=fetch_user_all())
+
+        password = request.form['password']
+        if not password.isalnum():
+            return render_template("login.html", data=fetch_user_all())
+
+        try:
+            register = request.form['register']
+        except:
+            register = None
+
+        if register is not None:
+            create_user(username, password)
+            session['User'] = fetch_user_id(username)[0]['Id']
+            return redirect(url_for('blog'))
+        else:
+            for user in fetch_user_all():
+                if user['UserName'] == username and user['Password'] == password:
+                    session['User'] = fetch_user_id(username)[0]['Id']
+                    return redirect(url_for('blog'))
+    return render_template("login.html", data=fetch_user_all())
+
+
+
+if __name__ == "__main__":
+    # init_db()
+    # FOR SESSION
+    app.secret_key = os.urandom(24)
+    app.run()
+else:
+    print("is being imported into another module")
